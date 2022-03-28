1c155e5fac5e543bb5409951ae4a8eef7c5cdc2e
usfbelhadj <usf.belhadj@gmail.com>
2022-03-28 16:26:10 +0100

    test

--- 0x03-asynchronous/breaking_bad.dart
+++ 0x03-asynchronous/breaking_bad.dart
@@ -1,20 +1,17 @@
-import 'package:http/http.dart' as http;
+import "package:http/http.dart" as http;
 import 'dart:async';
 import 'dart:convert';
 
 Future<void> printBbCharacters() async {
-  final response = await http.get(
-    Uri.parse('https://www.breakingbadapi.com/api/characters'),
-  );
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response, then parse the JSON.
+  try {
+    final response = await http.get(
+      Uri.parse('https://www.breakingbadapi.com/api/characters'),
+    );
     var resJson = jsonDecode(response.body);
     for (var i = 0; i < resJson.length; i++) {
       print(resJson[i]["name"]);
     }
-  } else {
-    // If the server did not return a 200 OK response, then throw an exception.
-    throw Exception('Failed to load album');
+  } catch (e) {
+    print(e);
   }
 }
