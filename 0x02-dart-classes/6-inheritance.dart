d450a1003a9dc8367d6f35a70d3b1de7c748a1d9
usfbelhadj <usf.belhadj@gmail.com>
2022-05-27 13:18:41 +0100

    Test

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -29,6 +29,6 @@ class User extends Password {
   @override
   String toString() {
     final pass = Password(password: user_password);
-    return 'User(id : $id ,name: $name,  age: $age, height: $height, Password: ${pass.isValid()})';
+    return 'User(id : $id ,name: $name, age: $age, height: $height, Password: ${pass.isValid()})';
   }
 }
